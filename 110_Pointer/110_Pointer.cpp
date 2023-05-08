2416f90afd36a87151c4b7b333a55c3abf8e075a
azzamaflah <azzamhebat46@gmail.com>
2023-05-08 10:48:27 +0700

    membuat kelas mahasiswa untuk menampilkan nim

--- 110_Pointer/110_Pointer.cpp
+++ 110_Pointer/110_Pointer.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
+class mahasiswa {
+    public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
